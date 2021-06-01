37ea216395f995d54949849546af9973c19db254
ClaireEsnault <c.esnault@rns.ecolescreatives.com>
2021-06-01 22:28:40 +0200

    test

--- Menu.js
+++ Menu.js
@@ -6,20 +6,38 @@ class Menu extends Phaser.Scene{
 
 preload()
 {
-    this.load.image('menu', 'assets/menu.png');
+    var bouton1;
+    var bouton2;
+    var bouton3;
+
+
+    this.load.image('bouton1', 'assets/bouton_jouer.png');
+    this.load.image('bouton2', 'assets/bouton_options.png');
+    this.load.image ('bouton3', 'assets/bouton_quitter.png');
+    this.load.image('menu', 'assets/fond_menu.png');
 }
 //Fin preload
 //_________________________________________________________________________________________________________________________
 
 create()
-{
+{   
     this.add.image(448,224,'menu');
+    this.add.image(150,50, 'bouton1');
+    this.add.image(150,150, 'bouton2');
+    this.add.image(150,250, 'bouton3');
+
+    this.input.once('pointerdown', function (event)
+    {
+        this.scene.start('Niveau3');
+    }, this);
+    
 }
 //Fin create
 //_________________________________________________________________________________________________________________________
 
 update()
 {
+
 
 }
 //Fin update
--- Niveau3.js
+++ Niveau3.js
@@ -6,21 +6,59 @@ class Niveau3 extends Phaser.Scene{
 
 preload()
 {
-
+    this.load.image('fond', 'assets/fond_niveau.png');
+    this.load.image('perso', 'assets/perso.png');
+    this.load.image('ennemi', 'assets/ennemi.png');
+    this.load.image('bordure', 'assets/bordure.png');
 }
 //Fin preload
 //_________________________________________________________________________________________________________________________
 
 create()
 {
+    this.add.image(2500, 224, 'fond');
 
+    this.player = this.physics.add.image(150,50, 'perso');
+    this.player.direction = 'right';
+    this.player.setBounce(0);
+    this.player.setCollideWorldBounds(true);
+
+    //this.ennemi = this.physics.add.image(400, 50, 'ennemi');
+    //this.ennemi.setCollideWorldBounds(true);
+    
+    //this.platform = this.physics.add.image(867, 447, 'bordure')
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+
+    this.cameras.main.setBounds(0, 0, 5000, 448);
+    this.cameras.main.setSize(896,448);
+    this.cameras.main.startFollow (this.player);
 }
 //Fin create
 //_________________________________________________________________________________________________________________________
 
 update()
 {
-
+    if (this.cursors.left.isDown)
+    {
+        this.player.direction = 'left';
+        this.player.setVelocityX(-300);
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.player.direction = 'right';
+        this.player.setVelocityX(300);
+    }
+    else if(this.cursors.left.isUp && this.cursors.right.isUp)
+    {
+        this.player.setVelocityX(0);
+    }
+    if (this.cursors.up.isDown)
+    {
+        this.player.direction = 'up';
+        this.player.setVelocityY(-300);
+    }
+  
 }
 //Fin update
 //_________________________________________________________________________________________________________________________
